a0743628f2489bdd9185ed10d4fa198d47ef6ae8
CoolPerson5324 <108703714+CoolPreson5324@users.noreply.github.com>
2024-03-07 12:13:38 -0800

    Update Controller.kt
    just adding comments so others can understand what this does

--- src/main/java/frc/robot/Controller.kt
+++ src/main/java/frc/robot/Controller.kt
@@ -10,11 +10,11 @@ object Controller {
     init {
         object: Thread() {
             override fun run() {
-                while (true) {
+                while (true) {  //if compress is true and sees if the button pressed its equal to 3 and sets compress to false
                     if (j1.getRawButtonReleased(3)) {
                         compress = !compress
                     }
-                    sleep(20)
+                    sleep(20) //this waits 20 seconds in real time before starting
                 }
             }
         }.start()
@@ -25,11 +25,11 @@ object Controller {
     init {
         object: Thread() {
             override fun run() {
-                while (true) {
+                while (true) { //if extends is false when the button is released its equal to 4 and sets extends to true
                     if (j1.getRawButtonReleased(4)) {
                         extends = !extends
                     }
-                    sleep(20)
+                    sleep(20) //this waits 20 seconds in real time before starting
                 }
             }
         }.start()
